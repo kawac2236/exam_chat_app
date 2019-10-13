834ea6003d8d6bc28088f26b259880fd62e5c07d
kawac2236 <kawac2236@gmail.com>
2019-10-13 19:50:00 +0900

    action cable refs #15

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,17 +1,31 @@
 App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
+    console.log('conected');
   },
 
   disconnected: function() {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
+  received: function(message) {
+    console.log('receive')
+    const messages = document.getElementById('messages')
+    messages.innerHTML += message
     // Called when there's incoming data on the websocket for this channel
   },
 
   speak: function() {
-    return this.perform('speak');
+    return this.perform('speak', {message: content});
   }
 });
+
+document.addEventListener('DOMContentLoaded',function(){
+  input = document.getElementById('chat-input')
+  button = document.getElementById('button')
+  button.addEventListener('click', function(){
+    content = input.value
+    App.room.speak(content)
+    input.value = ''
+  })
+})
--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,12 +1,19 @@
 class RoomChannel < ApplicationCable::Channel
+  # 監視する
   def subscribed
-    # stream_from "some_channel"
+    stream_from "room_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    # frontからのデータを受け取って、DBに格納する
+    message = Message.create!(content: data['message'])
+    template = ApplicationController.renderer.render(partial: 'messages/message', locals: {message: message})
+    #  frontにbroadcastする。送信先は,room.jsのRoomChannel
+    ActionCable.server.broadcast 'room_channel',template 
+    # binding.pry
   end
 end
